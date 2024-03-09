9e50047c6fa3efeee8465f9c7b81860184261e1d
muhammadsami <muhammadsami1242@gmail.com>
2024-03-09 20:16:40 +0500

    setup created

---
+++ app/layout.js
@@ -0,0 +1,26 @@
+
+import "./globals.css";
+
+
+
+export const metadata = {
+  title: "Prompt Genertor",
+  description: "Discover & Share A.I Prompts",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body >
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          {children}
+
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+
+    <section className="w-full flex-col flex items-center">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">A.I Powered Prompts</span>
+
+      </h1>
+      <p className="desc text-center">Prompt Genertor is an open-source A.I prompting tool for modern world to discover , create and share creative prompts. </p>
+    </section>
+  );
+
+}
